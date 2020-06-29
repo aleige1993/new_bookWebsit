2c816677f9cc5b4397771c287792294088b7c4ae
aleige1993 <927403622@qq.com>
2020-06-29 22:59:42 +0800

    updata

--- src/components/headers/index.vue
+++ src/components/headers/index.vue
@@ -1,157 +1,149 @@
 <template>
   <div>
-     <div class="allpage">
-    <div class="main ">
-      <div class="left_panl">
-        <div class="tupianbox">
-          <img src="../../assets/image_sj_jcys.png" alt />
-          <div class="ziduan">
-            <p>全国中小学图书装备</p>
-            <p>公共服务平台</p>
+    <div class="allpage">
+      <div class="main">
+        <div class="left_panl">
+          <div class="tupianbox">
+            <img src="../../assets/image_sj_jcys.png" alt />
+            <div class="ziduan">
+              <p>全国中小学图书装备</p>
+              <p>公共服务平台</p>
+            </div>
+          </div>
+          <div class="inputbox">
+            <input type="text" placeholder="输入搜索内容" />
+            <img class="soso" src="../../assets/soso.png" />
           </div>
         </div>
-        <div class="inputbox">
-          <input type="text" placeholder="输入搜索内容" />
-          <img class="soso" src="../../assets/soso.png" />
+      </div>
+    </div>
+    <div class="bures">
+      <div class="main cents">
+        <div class="nav_left">
+          <li
+            class="links"
+            @click="changNav(index)"
+            :class="index==indexAT?'actives':''"
+            v-for="(item,index) in  linkData"
+            :key="index"
+          >
+            <router-link :to="item.link">
+              <img :src="index==indexAT?item.imgactive:item.img" />
+              {{item.name}}
+            </router-link>
+          </li>
+        </div>
+        <div class="nav_right">
+          <a>登录</a>
+          <span>|</span>
+          <a>注册</a>
         </div>
       </div>
     </div>
   </div>
-  <div class="bures">
-    <div class="main cents">
-        <div class="nav_left">
-          <li class="links" @click="changNav(index)" :class="index==indexAT?'actives':''" v-for="(item,index) in  linkData" :key="index">
-            <img :src="index==indexAT?item.imgactive:item.img" />
-            <router-link  :to="item.link" >{{item.name}}</router-link>
-            </li>
-            <!-- <li class="links">
-            <img src="../../assets/fagui.png" />
-            <router-link  to="/" >政策法规</router-link>
-            </li>
-            <li class="links">
-            <img src="../../assets/gonggao.png" />
-            <router-link  to="/" >信息公告</router-link>
-            </li>
-            <li class="links">
-            <img src="../../assets/tuijian.png" />
-            <router-link  to="/" >推荐书目</router-link>
-            </li>
-            <li class="links">
-            <img src="../../assets/zhanshi.png" />
-            <router-link  to="/" >信息展示</router-link>
-            </li> -->
-        </div>
-        <div class="nav_right">
-            <a>登录</a> <span>|</span> <a>注册</a>
-        </div>
-    </div>
-  </div>
-
-
-  </div>
 </template>
 <script>
 export default {
-   data () {
-      return {
-        indexAT:0,
-        linkData:[
-          {
-            img: require('../../assets/home.png'),
-            imgactive: require('../../assets/home_2.png'),
-            name:'首页',
-            index:0,
-            link:'/home'
-          },
-          {
-            img:require('../../assets/fagui.png'),
-            imgactive: require('../../assets/fagui_2.png'),
-            name:'政策法规',
-            index:1,
-            link:'/InfoList'
-          },
-          {
-            img: require('../../assets/gonggao.png'),
-            imgactive: require('../../assets/gonggao_2.png'),
-            name:'信息公告',
-            index:2,
-            link:'/InfoList'
-          },
-          {
-            img: require('../../assets/tuijian.png'),
-            imgactive: require('../../assets/tuijian_2.png'),
-            name:'推荐书目',
-            index:3,
-            link:'/InfoList'
-          },
-          {
-            img: require('../../assets/zhanshi.png'),
-            imgactive: require('../../assets/zhanshi_2.png'),
-            name:'信息展示',
-            index:4,
-            link:'/InfoList'
-          }
-        ]
-      };
-    },
-     components: {},
+  data() {
+    return {
+      indexAT: 0,
+      linkData: [
+        {
+          img: require("../../assets/home.png"),
+          imgactive: require("../../assets/home_2.png"),
+          name: "首页",
+          index: 0,
+          link: "/home"
+        },
+        {
+          img: require("../../assets/fagui.png"),
+          imgactive: require("../../assets/fagui_2.png"),
+          name: "政策法规",
+          index: 1,
+          link: "/InfoList"
+        },
+        {
+          img: require("../../assets/gonggao.png"),
+          imgactive: require("../../assets/gonggao_2.png"),
+          name: "信息公告",
+          index: 2,
+          link: "/InfoList"
+        },
+        {
+          img: require("../../assets/tuijian.png"),
+          imgactive: require("../../assets/tuijian_2.png"),
+          name: "推荐书目",
+          index: 3,
+          link: "/recommend"
+        },
+        {
+          img: require("../../assets/zhanshi.png"),
+          imgactive: require("../../assets/zhanshi_2.png"),
+          name: "信息展示",
+          index: 4,
+          link: "/InfoList"
+        }
+      ]
+    };
+  },
+  components: {},
 
-    computed: {},
+  computed: {},
 
-    beforeMount() {},
+  beforeMount() {},
 
-    mounted() {},
+  mounted() {},
 
-    methods: {
-      changNav(i){
-        this.$data.indexAT = i
-      }
-    },
+  methods: {
+    changNav(i) {
+      this.$data.indexAT = i;
+    }
+  },
 
-    watch: {}
+  watch: {}
 };
 </script>
 
 <style  scoped>
-.nav_right a{
+.nav_right a {
   color: #fff;
   cursor: pointer;
 }
-.nav_right span{
-  margin:0 10px;
+.nav_right span {
+  margin: 0 10px;
   font-size: 18px;
   color: #fff;
 }
-.nav_right{
+.nav_right {
   font-size: 20px;
   color: #fff;
 }
-.cents{
+.cents {
   display: flex;
   justify-content: space-between;
   align-items: center;
   align-content: center;
   height: 88px;
 }
-li.actives a{
-  color: #C4131C;
+li.actives a {
+  color: #c4131c;
   font-weight: 600;
 }
-.links{
+.links {
   display: inline-block;
   margin-right: 50px;
 }
-.links:nth-child(3) img{
+.links:nth-child(3) img {
   margin-top: 3px;
 }
-.links img{
+.links img {
   float: left;
-  vertical-align:middle;
+  vertical-align: middle;
   margin-right: 4px;
 }
 
-.links a{
-  font-size:20px;
+.links a {
+  font-size: 20px;
   color: #fff;
 }
 .allpage {
@@ -160,67 +152,69 @@ li.actives a{
   height: 300px;
   width: 100%;
 }
-.left_panl{
-
+.left_panl {
 }
-.tupianbox{
+.tupianbox {
   display: flex;
   padding-top: 60px;
 }
-.ziduan{
+.ziduan {
   font-size: 20px;
-  font-weight:600;
+  font-weight: 600;
   line-height: 30px;
   margin-left: 10px;
   letter-spacing: 3px;
 }
-.inputbox{
+.inputbox {
   width: 353px;
   height: 50px;
-  border: 2px solid #6599F7;
-  border-radius:25px;
-  background:rgba(221,221,221,0.4);
-  overflow:hidden;
+  border: 2px solid #6599f7;
+  border-radius: 25px;
+  background: rgba(221, 221, 221, 0.4);
+  overflow: hidden;
   padding: 5px;
   margin-top: 80px;
 }
-.inputbox input{
+.inputbox input {
   width: 85%;
-  height:100%;
-  background:rgba(221,221,221,0.1);
-  border:none;
-  outline:none;
+  height: 100%;
+  background: rgba(221, 221, 221, 0.1);
+  border: none;
+  outline: none;
   float: left;
   text-indent: 20px;
-  color: #6599F7;
+  color: #6599f7;
   font-size: 16px;
 }
-.inputbox .soso{
+.inputbox .soso {
   float: right;
-  margin-top:10px;
+  margin-top: 10px;
   margin-right: 15px;
 }
-input::-webkit-input-placeholder { 
-/* WebKit browsers */ 
-color: #6599F7; 
-} 
-input:-moz-placeholder { 
-/* Mozilla Firefox 4 to 18 */ 
-color: #6599F7; 
-} 
-input::-moz-placeholder { 
-/* Mozilla Firefox 19+ */ 
-color: #6599F7; 
-} 
-input:-ms-input-placeholder { 
-/* Internet Explorer 10+ */ 
-color: #6599F7; 
+input::-webkit-input-placeholder {
+  /* WebKit browsers */
+  color: #6599f7;
 }
-.bures{
-  width:100%;
-  height:88px;
-  background:linear-gradient(180deg,rgba(111,166,255,1) 0%,rgba(75,118,227,1) 100%);
-  opacity:1;
+input:-moz-placeholder {
+  /* Mozilla Firefox 4 to 18 */
+  color: #6599f7;
 }
-
+input::-moz-placeholder {
+  /* Mozilla Firefox 19+ */
+  color: #6599f7;
+}
+input:-ms-input-placeholder {
+  /* Internet Explorer 10+ */
+  color: #6599f7;
+}
+.bures {
+  width: 100%;
+  height: 88px;
+  background: linear-gradient(
+    180deg,
+    rgba(111, 166, 255, 1) 0%,
+    rgba(75, 118, 227, 1) 100%
+  );
+  opacity: 1;
+}
 </style>
